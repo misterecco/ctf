48c5afc7ca8d8f7499c6292bbd081ea4546d8620
Tomasz Kepa <tomek@qed.ai>
2017-05-29 11:57:32 +0200

    Task #4: some scribbles, still far from solving even the easy part

--- 04_crypto/task_04.py
+++ 04_crypto/task_04.py
@@ -5,6 +5,8 @@ import sys
 
 sys.setrecursionlimit(10000)
 
+FLAG_NUMBERS = [0, 1, 123, 0x7F4B81BE, 0xDB3BE6B0, 0xFFFFFFFF]
+
 
 class Decryptor():
     def __init__(self):
@@ -23,6 +25,11 @@ class Decryptor():
 
         enc_zero = snd_cand_0 if first_cand_0 == enc_one else first_cand_0
 
+        self.p.send('{}+{}\n'.format(enc_zero, enc_zero))
+        zero = int(self.p.recvline())
+
+        assert(enc_zero == zero)
+
         self.encodings = {
             0: enc_zero,
             1: enc_one,
@@ -39,17 +46,24 @@ class Decryptor():
         if num in self.encodings:
             return self.encodings[num]
         else:
-            first = int(math.log(num, 2))
-            snd = num - first
-            ef = self.encrypt_number(first)
-            es = self.encrypt_number(snd)
-            en = self.p.send("{}+{}\n".format(ef, es))
-            en = int(self.p.recvline())
-            self.encodings[num] = en
-            return en
+            fst = 2 ** int(math.log(num, 2))            
+            enq = [(fst, num-fst)]
+            while len(enq) > 0:
+                (f, s) = enq[-1]
+                if s in self.encodings and (f+s) not in self.encodings:
+                    ef = self.encrypt_number(f)
+                    es = self.encrypt_number(s)
+                    self.p.send("{}+{}\n".format(ef, es))
+                    en = int(self.p.recvline())
+                    self.encodings[f+s] = en
+                    enq.pop()
+                elif s not in self.encodings:
+                    fst = 2 ** int(math.log(s, 2))
+                    enq.append((fst, s-fst))
+            return self.encodings[num]
 
     def prepare_numbers_for_first_flag(self):
-        for num in [0, 1, 123, 0x7F4B81BE, 0xDB3BE6B0, 0xFFFFFFFF]:
+        for num in FLAG_NUMBERS:
             self.encrypt_number(num)
 
     def finish(self):
@@ -57,18 +71,31 @@ class Decryptor():
 
         count = self.p.recvline()
         print(count)
-        self.p.recvline()
+        a = self.p.recvline()
+        print(a)
+
+        for i in FLAG_NUMBERS:
+            a = self.p.recvuntil(" == ")
+            print(a)
+            print("Number: {} -> {}".format(i, self.encodings[i]))
+            ans = "{}\n".format(self.encodings[i])
+            self.p.send(ans)
+            s = self.p.recvall()
+            print(s)
+        
+        res = self.p.recvall()
+        print(res)
 
 
 def solve():
     d = Decryptor()
 
-    print(d.encodings)
+    # print(d.encodings)
     d.prepare_encodings()
-    print(d.encodings)
+    # print(d.encodings)
 
     d.prepare_numbers_for_first_flag()
-    print(d.encodings)
+    # print(d.encodings)
 
     d.finish()
 
